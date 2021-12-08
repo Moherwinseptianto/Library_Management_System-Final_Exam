fa7506e6fe53380fd84816fb3d24dafb2641e6b8
Juan Sebastian Gultom <70578691+JuanSGltm@users.noreply.github.com>
2021-12-08 15:28:10 +0700

    Add files via upload

--- Librarian.cpp
+++ Librarian.cpp
@@ -28,6 +28,9 @@ class Librarian {
         void modify();
         void getdata();
         int branch();
+        void issue();
+        void fine(int d, int m, int y, int dd, int mm, int yy);
+        void der(char st[], int b, int x);
 };
 
 //PROGRAM TO HOME PAGE OF LIBRARIAN
@@ -39,7 +42,8 @@ void Librarian::open(){
     cout << "\n\t1.View Booklist\n";
     cout << "\n\t2.Search For Book\n";
     cout << "\n\t3.Modify/Add Book in Booklist\n";
-    cout << "\n\t4.Close Aplication\n";
+    cout << "\n\t4.Issue Book\n";
+    cout << "\n\t5.Close Aplication\n";
 
     cout << "\n\n\tEnter your choice: ";
 
@@ -65,8 +69,13 @@ void Librarian::open(){
         modify();
     }
 
-    //Force close
+    //Issue the book, view the issue book, reissue book, return book.
     else if (i==4){
+        system("cls");
+        issue();
+    }
+    //Force close
+    else if (i==5){
         exit(0);
     }
 
@@ -478,9 +487,256 @@ void Librarian::getdata(){
     cin >> q; 
 }
 
-//Program to issue problem
+//PROGRAM TO ISSUE THE BOOK
+void Librarian::issue() {
+    char st[50], st1[20]; //student ID and book ID variable (for reissue)
+    int b,i,j;
+    int d,m,y, dd,mm,yy; //For date
+    int cont=0;
 
-// To know what category the student want
+    system("cls");
+    cout << "\n\t\tPlease Choose One Option\n";
+    cout << "\n\n\t\t1. Issue Book";
+    cout << "\n\n\t\t2. View Issue Book";
+    cout << "\n\n\t\t3. Reissue Book";
+    cout << "\n\n\t\t4. Return Book";
+    cout << "\n\n\t\t5. Go Back to Home Page";
+
+    cout << "\n\n\n\t\tEnter your choice : ";
+    cin >> i;
+
+    fflush(stdin);
+
+    //Issue Book (Student Borrowing Book)
+    if (i==1) {
+        system("cls");
+        b = branch();
+        system("cls");
+        fflush(stdin);
+        cout << "\n\n\t\t->Please Enter the Detail<-";
+
+        cout << "\n\n\t\tEnter Bookname : ";
+        cin.getline(bookname,100);
+
+        cout << "\n\n\t\tEnter Book's ID : ";
+        cin.getline(sc,20);
+        der(sc, b, 1);
+
+        cout << "\n\n\t\tEnter Student's Name : ";
+        cin.getline(authorname, 100); //using authorname variable more efficient than making new variable studentname
+
+        cout << "\n\n\t\tEnter Student's ID : ";
+        cin.getline(sc1, 20); //using sc1 (book's id variable) to efficiency
+
+        cout << "\n\n\t\tEnter date (dd mm yy) : ";
+        cin >> q >> B >> p; //more efficient using exist variable
+
+        ofstream outf("student.txt", ios::binary|ios::app);
+        outf.write((char*)this,sizeof(*this));
+        outf.close();
+        cout << "\n\n\n\t\t\tIssue Successfully";
+    }
+
+    //Listing the detail issue Book
+    if (i==2) {
+        ifstream intf("student.txt", ios::binary|ios::app);
+        system("cls");
+        cout << "\n\n\t\t->Issue Detail<-";
+        intf.read((char*)this,sizeof(*this));
+        i = 0;
+        while(!intf.eof()) {
+            i++;
+            cout << "\n\t\t********** " << i << "**********\n";
+            cout << "\n\t\tStudent Name : " << authorname;
+            cout << "\n\t\tStudent's ID : " << sc1;
+            cout << "\n\t\tBook Name/Title : " << bookname;
+            cout << "\n\t\tBook's ID : " << sc;
+            cout << "\n\t\tDate : " << q << "/" << B << "/" << p << endl;
+            intf.read((char*)this,sizeof(*this));
+        }
+        intf.close();
+    }
+
+    //Reiissue the book, if late there is a fine to pay
+    else if (i == 3) {
+        system ("cls");
+        fflush(stdin);
+
+        cout << "\n\n\t\t->Please Enter the Detail<-";
+
+        cout << "\n\n\t\tEnter Student's ID : ";
+        cin.getline(st, 50);
+
+        cout << "\n\n\t\tEnter Book's ID : ";
+        cin.getline(st1, 20);
+
+        fstream intf("student.txt", ios::in|ios::out|ios::ate|ios::binary);
+        intf.seekg(0);
+        intf.read((char*)this,sizeof(*this));
+        while(!intf.eof()){
+            for(i=0; sc[i]!='\0' && st1[i]!='\0' && st1[i]==sc[i]; i++);
+                for(j=0; sc1[j]!='\0' && st[j]!='\0' && st[j]==sc1[j]; j++);
+                    if(sc[i]=='\0' && sc1[j]=='\0' && st[j]=='\0' && st1[i]=='\0') {
+                        d=q;
+                        m=B;
+                        y=p;
+                        cout<<"\n\t\tEnter New Date : ";
+                        cin>>q>>B>>p;
+                        fine(d,m,y,q,B,p); //fn1
+                        int u = intf.tellp();
+                        intf.seekp(u - sizeof(*this)); //fn3
+                        intf.write((char*)this,sizeof(*this)); //fn5
+                        cout<<"\n\n\t\tReissue successfully."; //fn3
+                        break;
+                }
+            intf.read((char*)this,sizeof(*this));
+        }
+        intf.close();
+    }
+
+    //
+    else if (i==4) {
+        system("cls");
+        b=branch();
+        system("cls");
+        fflush(stdin);
+
+        cout << "\n\n\t\t->Please Enter the Detail<-";
+
+        cout << "\n\n\t\tEnter Book's ID : ";
+        cin.getline(st1, 20);
+        der(st1, b, 2);
+
+        cout << "\n\n\t\tEnter Student's ID : ";
+        cin.getline(st, 50);
+
+        cout << "\n\n\t\tEnter Present Date : ";
+        cin >> d >> m >> y;
+
+        ofstream outf("temp.txt", ios::app|ios::binary);
+        ifstream intf("student.txt", ios::binary);
+        intf.read((char*)this,sizeof(*this));
+        while(!intf.eof()){
+            for(i=0; sc[i]!='\0' && st1[i]!='\0' && st1[i]==sc[i]; i++);
+                for(j=0; sc1[j]!='\0' && st[j]!='\0' && st[j]==sc1[j]; j++);
+                    if(sc[i]=='\0' && sc1[j]=='\0' && st[j]=='\0' && st1[i]=='\0' && cont==0) {
+                        cont++;
+                        intf.read((char*)this,sizeof(*this));
+                        fine(q,B,p,d,m,y);
+                        cout<<"\n\t\tReturned successfully.";
+                    }
+
+                    else {
+                        outf.write((char*)this,sizeof(*this));
+                        intf.read((char*)this,sizeof(*this));
+                    }
+        }
+
+        intf.close();
+        outf.close();
+        getch();
+        remove("student.txt");
+        rename("temp.txt","student.txt");
+    }
+
+    else if(i==5) {
+        system("cls");
+        open();
+    }
+
+    else {
+        cout << "\n\t\tWrong Input...";
+    }
+
+    cout << "\n\n\t\tPress any key to continue...";
+    getch();
+    system("cls");
+    open(); 
+}
+
+// To calculate the fine (fine is for reissue or return late)
+void Librarian::fine(int d, int m, int y, int dd, int mm, int yy){
+    long int n1, n2;
+    int difference;
+    int years, l, i;
+    const int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    //count for how many days
+    n1 = y*365 + d;
+    for (i=0; i < m-1; i++) {
+        n1 += monthDays[i]; //fn1353
+    }
+    years = y;
+
+    //leap year
+    if(m <= 2) {
+        years--;
+    }
+    l = years / 4 - years / 100 + years / 400;
+
+    n1 += l;        //days
+
+    //count days for the new date
+    n2 = yy*365 + dd;
+    for (i=0; i<mm-1; i++) {
+        n2 += monthDays[i];
+    }
+    years == yy;
+
+    if (m<=2){
+        years = yy;
+    }
+    l = years / 4 - years / 100 + years / 400;
+
+    n2 += l;        //days
+
+    n1 = n2-n1;     //the differences of days
+
+    //fine
+    n2 = n1-15;
+
+    if (n2>0) {
+        cout << "\n\t\tTotal Fine is : " << n2;
+    }
+}
+
+//To Decrease the quantity the book
+void Librarian::der(char st[], int b, int x){
+    int i,cont=0;
+
+    fstream intf("Bookdata.txt",ios::in|ios::out|ios::ate|ios::binary);
+    intf.seekg(0);
+    intf.read((char*)this,sizeof(*this));
+
+    while(!intf.eof()) {
+        for(i=0;b==B&&sc[i]!='\0'&&st[i]!='\0'&&st[i]==sc[i];i++);
+            if(sc[i]=='\0'&&st[i]=='\0') {
+                cont++;
+                if(x==1) {
+                    q--;
+                }
+                else {
+                    q++;
+                }
+            int u = intf.tellp();
+            intf.seekp(u - sizeof(*this));
+            intf.write((char*)this,sizeof(*this));
+            break;
+            }
+        intf.read((char*)this,sizeof(*this));
+    }
+    if (cont==0) {
+        cout << "\n\t\t Book not Found\n";
+        cout << "\n\n\t\tPress any key to continue...";
+        getch();
+        system("cls");
+        issue();
+    }
+    intf.close();
+}
+
+
+// To open category of the book
 int Librarian::branch()
 {
     int i;
